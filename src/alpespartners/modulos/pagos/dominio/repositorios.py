bade4c06a8c760ce9139d05c93aed4dab5a48483
JhormanGalindo <j.galindop@uniandes.edu.co>
2025-09-06 13:28:01 -0500

    fix(correct monto assignment in MapeadorPago)

---
+++ src/alpespartners/modulos/pagos/dominio/repositorios.py
@@ -0,0 +1,13 @@
+""" Interfaces para los repositorios del dominio de pagos
+
+En este archivo usted encontrará las diferentes interfaces para repositorios
+del dominio de pagos
+
+"""
+
+from abc import ABC
+from alpespartners.seedwork.dominio.repositorios import Repositorio
+
+
+class RepositorioPagos(Repositorio, ABC):
+    ...
---
+++ src/alpespartners/modulos/pagos/infraestructura/repositorios.py
@@ -0,0 +1,41 @@
+""" Repositorios para el manejo de persistencia de objetos de dominio en la capa de infrastructura del dominio de vuelos
+
+En este archivo usted encontrará las diferentes repositorios para
+persistir objetos dominio (agregaciones) en la capa de infraestructura del dominio de vuelos
+
+"""
+
+from uuid import UUID
+
+from alpespartners.config.db import db
+
+from alpespartners.modulos.pagos.dominio.entidades import Pago
+from alpespartners.modulos.pagos.dominio.repositorios import RepositorioPagos
+from alpespartners.modulos.pagos.dominio.fabricas import FabricaPagos
+
+from .mapeadores import MapeadorPago
+
+
+class RepositorioReservasSQLAlchemy(RepositorioPagos):
+    def __init__(self):
+        self.fabrica_pagos: FabricaPagos = FabricaPagos()
+
+    def obtener_por_id(self, id: UUID) -> Pago:
+        # TODO
+        raise NotImplementedError
+
+    def obtener_todos(self) -> list[Pago]:
+        # TODO
+        raise NotImplementedError
+
+    def agregar(self, pago: Pago):
+        pago_dto = self.fabrica_pagos.crear_objeto(pago, MapeadorPago())
+        db.session.add(pago_dto)
+
+    def actualizar(self, pago: Pago):
+        # TODO
+        raise NotImplementedError
+
+    def eliminar(self, reserva_id: UUID):
+        # TODO
+        raise NotImplementedError
