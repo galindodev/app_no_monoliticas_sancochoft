74bb9f037ee3692ba33dfa005f7dc223fae48c41
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-09-14 12:22:22 -0500

    refactor(pagos): delete unused endpoint

--- pagos/api/pagos.py
+++ pagos/api/pagos.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import request, Response
+from flask import Response
 
 from pagos.modulos.pagos.aplicacion.queries.obtener_pagos_pendientes import ObtenerPagosPendientes
 from pagos.seedwork.aplicacion.queries import ejecutar_query
@@ -9,30 +9,8 @@ from pagos.seedwork.dominio.excepciones import ExcepcionDominio
 
 from pagos.modulos.pagos.aplicacion.mapeadores import MapeadorPagoDTOJson
 
-from pagos.modulos.pagos.aplicacion.comandos.solicitar_pago import SolicitarPago
-from pagos.modulos.pagos.infraestructura.despachadores import Despachador
-
 
 bp = api.crear_blueprint('pagos', '/pagos')
-
-
-# TODO: remove this endpoint
-@bp.route('/solicitar-pago-comando', methods=('POST',))
-def solicitar_pago_asincrona():
-    try:
-        solicitud_dict = request.json
-
-        map_solicitud = MapeadorPagoDTOJson()
-        solicitud_dto = map_solicitud.externo_a_dto(solicitud_dict)
-
-        comando = SolicitarPago(id_influencer=solicitud_dto.id_influencer, monto=solicitud_dto.monto)
-
-        despachador = Despachador()
-        despachador.publicar_comando(comando, topico='comandos-pagos')
-
-        return Response('{}', status=202, mimetype='application/json')
-    except ExcepcionDominio as e:
-        return Response(json.dumps(dict(error=str(e))), status=400, mimetype='application/json')
 
 
 @bp.route('/obtener-pagos-pendientes', methods=('GET',))
