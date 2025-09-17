5d324fc6fdcfcc97260c2cd05d25efc868b07690
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-09-17 17:59:04 -0500

    feat(sagas): listen other events

--- pagos/gunicorn_config.py
+++ pagos/gunicorn_config.py
@@ -1,9 +1,16 @@
-
 import logging
 from threading import Thread
 
-from pagos.modulos.pagos.infraestructura.consumidores import LiquidacionFinalizadaSuscripcion, SuscriptorSolicitarPago, LiquidacionFallidaSuscripcion
-from pagos.modulos.pagos.infraestructura.despachadores import PagoSolicitadoDispatcher, PagoPagadoDispatcher, PagoRechazadoDispatcher
+from pagos.modulos.pagos.infraestructura.consumidores import (
+    LiquidacionFinalizadaSuscripcion,
+    SuscriptorSolicitarPago,
+    LiquidacionFallidaSuscripcion,
+)
+from pagos.modulos.pagos.infraestructura.despachadores import (
+    PagoSolicitadoDispatcher,
+    PagoPagadoDispatcher,
+    PagoRechazadoDispatcher,
+)
 from pagos.modulos.sagas.infraestructura.consumidores import SagaEvent, SagaCommand
 from pagos.seedwork.infraestructura.consumidores import Subscriptor
 from pagos.seedwork.infraestructura.utils import register_esquemas
@@ -25,16 +32,35 @@ def post_fork(_, __):
     register_esquemas(PagoRechazadoDispatcher())
 
     # Comandos
-    Thread(target=escuchar_mensaje, args=(SuscriptorSolicitarPago(),), daemon=True).start()
+    Thread(
+        target=escuchar_mensaje, args=(SuscriptorSolicitarPago(),), daemon=True
+    ).start()
 
     # Eventos
-    Thread(target=escuchar_mensaje, args=(LiquidacionFinalizadaSuscripcion(),), daemon=True).start()
-    Thread(target=escuchar_mensaje, args=(LiquidacionFallidaSuscripcion(),), daemon=True).start()
+    Thread(
+        target=escuchar_mensaje, args=(LiquidacionFinalizadaSuscripcion(),), daemon=True
+    ).start()
+    Thread(
+        target=escuchar_mensaje, args=(LiquidacionFallidaSuscripcion(),), daemon=True
+    ).start()
 
     # Sagas
-    Thread(target=escuchar_mensaje, args=(SagaCommand.create('comandos-solicitar-pago'),), daemon=True).start()
+    Thread(
+        target=escuchar_mensaje,
+        args=(SagaCommand.create("comandos-solicitar-pago"),),
+        daemon=True,
+    ).start()
 
-    events_topics = ['eventos-pago-solicitado']
+    events_topics = [
+        "eventos-pago-solicitado",
+        "eventos-programa-completado",
+        "eventos-liquidacion-finalizada",
+        "eventos-liquidacion-fallida",
+        "eventos-pago-pagado",
+        "eventos-pago-rechazado",
+        "eventos-programa-completado",
+        "eventos-programa-reabierto",
+    ]
     for topic in events_topics:
         saga = SagaEvent.create(topic)
         Thread(target=escuchar_mensaje, args=(saga,), daemon=True).start()
